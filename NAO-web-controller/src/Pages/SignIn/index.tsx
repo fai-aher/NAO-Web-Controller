517c00aee19e539b4f6796ee04238edcaa9c485e
Gabriel Guevara <g.guevarab@uniandes.edu.co>
2024-10-17 18:14:10 -0500

    Welcome and SignIn Completed

--- NAO-web-controller/src/Pages/SignIn/index.tsx
+++ NAO-web-controller/src/Pages/SignIn/index.tsx
@@ -1,11 +1,84 @@
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+
 import './SignIn.css'
 
 function SignIn() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate();
+
+  const handleCancelClick = () => {
+    navigate('/');
+  }
+
+  const handleSignInClick = () => {
+
+    if (username === 'nao' && password === 'nao') {
+      navigate('/menu');
+    } else {
+      alert('Invalid username or password');
+    }
+  }
 
   return (
-    <div className='bg-red-100'>
-        <h1>SignIn Section</h1>
+    <div className="flex flex-col items-center justify-between h-screen bg-gray-100">
+
+      {/* Top Rectangle */}
+      <div className="w-[408px] h-[37px] bg-[#3C3C3C] rounded-[3px] justify-start "></div>
+
+      {/* Content */}
+      <div className="flex flex-row w-[510px] h-[200px] ml-20 mb-4 mt-10">
+        <img className="w-[170px] h-[130px] mt-3" src="nao-head.png" alt="NAO-Web-Controller Logo" />
+
+        <div className="flex flex-col ml-4 mt-4">
+
+          { /* Sign In Forms */}
+
+          <p className="text-[14px] font-bold mb-[1px]">Username</p>
+          <input type="text" placeholder="Please enter a registered username" 
+          className="w-[245px] h-[25px] text-[11px] mb-[13px] rounded-[3px]
+          border-[1px] border-black text-[#3C3C3C] px-1"
+          onChange={(e) => {setUsername(e.target.value)}}></input>
+
+          <p className="text-[14px] font-bold mb-[1px]">Password</p>
+          <input type="password" placeholder="Please enter the given password" 
+          className="w-[245px] h-[25px] text-[11px] rounded-[3px]
+          border-[1px] border-black text-[#3C3C3C] px-1"
+          onChange={(e) => {setPassword(e.target.value)}}></input>
+          
+
+          { /* Buttons */ }
+          <div className="flex flex-row space-x-[15px]">
+            <button className="w-[105px] h-[28px] mt-[26px] px-0 py-[1px] bg-[#D9D9D9] text-[#3C3C3C] rounded-[10px] font-bold
+            border-[1px] border-black text-[14px]" onClick={handleCancelClick} >Cancel</button>
+
+            <button className="w-[105px] h-[28px] mt-[26px] px-0 py-[1px] bg-[#3C3C3C] text-[#D9D9D9] rounded-[10px] font-bold
+            text-[14px]" onClick={handleSignInClick}>Sign in</button>
+          </div>
+      
+        </div>
+
+      </div>  
+
+      {/* The three astethic rounded squares */}
+      <div className="flex flex-row space-x-[28px] justify-end mb-[24px]">
+
+        <div className="w-[40px] h-[40px] bg-[#D9D9D9] rounded-[10px]"></div>
+        <div className="w-[40px] h-[40px] bg-[#D9D9D9] rounded-[10px]"></div>
+        <div className="w-[40px] h-[40px] bg-[#D9D9D9] rounded-[10px]"></div>
+
+      </div>
+
+      {/* Credits section */}
+      <div className="absolute bottom-0 right-0 p-4" >
+
+        <p className="text-[10px] text-right leading-[1.2] font-bold" >Made by: Alonso Hernandez Tavera <br/> @fai-aher (GitHub) </p>
+
+      </div>
+
     </div>
+
   )
 }
 
--- NAO-web-controller/src/Pages/Welcome/index.tsx
+++ NAO-web-controller/src/Pages/Welcome/index.tsx
@@ -1,11 +1,57 @@
+import { useNavigate } from 'react-router-dom'
+
 import './Welcome.css'
 
 function Welcome() {
 
+  const navigate = useNavigate();
+
+  const handleSignInClick = () => {
+
+    console.log('Sign in clicked');
+    navigate('/sign-in');
+
+  }
+
   return (
-    <div className='bg-red-100'>
-        <h1>Welcome Section</h1>
+
+    <div className="flex flex-col items-center justify-between h-screen bg-gray-100">
+
+      {/* Top Rectangle */}
+      <div className="w-[408px] h-[37px] bg-[#3C3C3C] rounded-[3px] justify-start "></div>
+
+      {/* Content */}
+      <div className="flex flex-row mt-0 w-[510px] h-[120px] ml-20 mb-4">
+        <img className="w-[170px] h-[130px]" src="nao-head.png" alt="NAO-Web-Controller Logo" />
+
+        <div className="flex flex-col ml-4 mt-4">
+          <h1 className="text-3x1 font-bold text-[24px] leading-[1.2]">Welcome to the <br/> NAO Web Remote Controller</h1>
+          <button className="w-[120px] h-[35px] mt-4 px-0 py-1.5 bg-[#3C3C3C] text-[#D9D9D9] rounded-[10px] font-bold
+           transition transform hover:scale-105" onClick={handleSignInClick}>Sign in</button>
+        </div>
+
+      </div>  
+
+      {/* The three astethic rounded squares */}
+      <div className="flex flex-row space-x-[28px] justify-end mb-[24px]">
+
+        <div className="w-[40px] h-[40px] bg-[#D9D9D9] rounded-[10px]"></div>
+        <div className="w-[40px] h-[40px] bg-[#D9D9D9] rounded-[10px]"></div>
+        <div className="w-[40px] h-[40px] bg-[#D9D9D9] rounded-[10px]"></div>
+
+      </div>
+
+      {/* Credits section */}
+      <div className="absolute bottom-0 right-0 p-4" >
+
+        <p className="text-[10px] text-right leading-[1.2] font-bold" >Made by: Alonso Hernandez Tavera <br/> @fai-aher (GitHub) </p>
+
+      </div>
+
     </div>
+
+
+
   )
 }
 
